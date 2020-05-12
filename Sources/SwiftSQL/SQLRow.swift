cef665587aaba010b87ef6108bbff2db7c49ed34
kean <grebenyuk.alexander@gmail.com>
2020-05-12 19:41:12 -0400

    Document SQLRow

--- Sources/SwiftSQL/SQLRow.swift
+++ Sources/SwiftSQL/SQLRow.swift
@@ -10,11 +10,23 @@ public struct SQLRow {
     let statement: SQLStatement // Storing as strong reference doesn't seem to affect performance
     var ref: OpaquePointer { statement.ref }
 
-    #warning("TODO: document")
+    /// Returns a single column of the current result row of a query.
+    ///
+    /// If the SQL statement does not currently point to a valid row, or if the
+    /// column index is out of range, the result is undefined.
+    ///
+    /// - parameter index: The leftmost column of the result set has the index 0.
     public subscript<T: SQLDataType>(index: Int) -> T {
         T.sqlColumn(statement: ref, index: Int32(index))
     }
 
+    /// Returns a single column of the current result row of a query. If the
+    /// value is `Null`, returns `nil.`
+    ///
+    /// If the SQL statement does not currently point to a valid row, or if the
+    /// column index is out of range, the result is undefined.
+    ///
+    /// - parameter index: The leftmost column of the result set has the index 0.
     public subscript<T: SQLDataType>(index: Int) -> T? {
         if sqlite3_column_type(ref, Int32(index)) == SQLITE_NULL {
             return nil
@@ -23,10 +35,12 @@ public struct SQLRow {
         }
     }
 
+    /// Returns a single column of the current result row of a query.
     public subscript<T: SQLDataType>(name: String) -> T {
         fatalError()
     }
 
+    /// Returns a single column of the current result row of a query.
     public subscript<T: SQLDataType>(name: String) -> T? {
         fatalError()
     }
